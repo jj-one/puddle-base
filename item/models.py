d96f478149136e4c531409b4d3c672a6b9d98512
james Agu <preagu@yahoo.com>
2023-09-11 17:39:18 +0100

    Created item app
    Created Category model
    
    Installed django-cleanup and Pillow

---
+++ item/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Category(models.Model):
+
+  name = models.CharField(max_length=255)
+
+  class Meta:
+    ordering = ('name',)
+
+  def __str__(self):
+    return self.name
+  
+# class Item(models.Model):
+
+#   category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='items')
+#   name = models.CharField(max_length=255)
+#   description = models.TextField(null=True, blank=True)
+#   price = models.FloatField()
+#   # image = models.ImageField(upload_to='item_images', null=True, blank=True)
+#   is_sold = models.BooleanField(default=False)
+#   created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='items')
+#   created = models.DateTimeField(auto_now_add=True)
