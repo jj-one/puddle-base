7f61ab4a9d53534fee32f3631100f370ad040ba7
james Agu <preagu@yahoo.com>
2023-09-11 19:21:03 +0100

    structured and populated front page with items and categories from database

--- core/views.py
+++ core/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from item.models import Category, Item
 
 # Create your views here.
 def index(request):
-  context = {}
+  categories = Category.objects.all()
+  items = Item.objects.filter(is_sold=False)[:6]
+  context = {'items': items, 'categories': categories}
   return render(request, 'core/index.html', context)
 
 def contact(request):
