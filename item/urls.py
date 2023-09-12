e0f385268217d6f7b59a0fbcfe1a481575ab80f9
james Agu <preagu@yahoo.com>
2023-09-12 11:38:54 +0100

    Created New Item form to enabled any logged in user to create and sell products by themselves

--- item/urls.py
+++ item/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import detail
+from .views import detail, new_item
 
 
 app_name = 'item'
 urlpatterns = [
+  path('new/', new_item, name='new'),
   path('<int:pk>/', detail, name='detail'),
 ]
--- item/views.py
+++ item/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Item
+from .forms import NewItemForm
 # Create your views here.
 
 def detail(request, pk):
@@ -12,3 +14,18 @@ def detail(request, pk):
   
   context = {"item": item, "related_items": related_items}
   return render(request, 'item/detail.html', context)
+
+@login_required
+def new_item(request):
+  form_type = "New Item"
+  if request.method == "POST":
+    form = NewItemForm(request.POST, request.FILES)
+    if form.is_valid():
+      item = form.save(commit=False)
+      item.created_by = request.user
+      item.save()
+      return redirect("item:detail", pk=item.id)
+  else:
+    form = NewItemForm()
+  context = {"form": form, "form_type": form_type}
+  return render(request, 'item/form.html', context)
